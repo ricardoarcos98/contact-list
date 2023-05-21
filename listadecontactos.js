6e41171eaefaad8a7042dda7a407897232c61f4d
Ricardo Arcos <ricardoarcos98@hotmail.com>
2023-05-21 13:55:28 -0500

    solucion real ejercicio 1

--- listadecontactos.js
+++ listadecontactos.js
@@ -1,37 +1,69 @@
-//Crea una lista de contactos con datos predefinidos, cada contacto debe contener el nombre y apellido como una sola cadena de caracteres
+ //Crea una lista de contactos con datos predefinidos, cada contacto debe contener el nombre y apellido como una sola cadena de caracteres
+ 
+ let contactList= [ "Ricardo Arcos", "Cristian ruis", "sofia rivera"];
 
-let listaContactos = ["Ricardo Arcos", "Laura castillejo", "Luciana Arcos"];
-console.log(listaContactos);
-//Crea una función para añadir un nuevo contacto a una lista.
-function agregar(nuevoContacto) {
-  listaContactos.push(nuevoContacto);
-  imprimirContactos();
-}
 
-console.log(listaContactos);
-//Crea una función para borrar un contacto existente de la lista
-function borrarContacto(nuevoContacto) {
-  const personDelete = listaContactos.indexOf(nuevoContacto);
-  if (personDelete !== -1) {
-    listaContactos.splice(personDelete, 1);
+ // Crea una función para añadir un nuevo contacto a una lista.
+
+ const addContact = function(newContact){
+    const contact= contactList.push(newContact);
+    return contact;
+  }
+
+  // Crea una función para borrar un contacto existente de la lista
+  const deleteContact = (newContact) => {
+  const personDelete = contactList.indexOf(newContact); 
+  if (personDelete !==-1){
+    contactList.splice(personDelete,1)
     console.log("Contacto borrado con éxito");
-  } else {
+  }else{
     console.log("El contacto no existe en la lista");
   }
-}
-console.log(listaContactos);
+  }
+  
+  // Otra forma de realizarlo 
+
+  // function deleteContact(newContact) {
+  //   const personDelete = contactList.indexOf(newContact);
+  //   if (personDelete !== -1) {
+  //     contactList.splice(personDelete, 1);
+  //     console.log("Contacto borrado con éxito");
+  //   } else {
+  //     console.log("El contacto no existe en la lista");
+  //   }
+  // }
+  
 // Crea una función para imprimir en consola los contactos presentes en la lista
 
-function imprimirContactos() {
-  console.log("Lista de contactos:");
-  listaContactos.forEach((contacto, indice) => {
-    console.log(`${indice + 1}. ${contacto}`);
-  });
-}
-// Imprimir contactos
-imprimirContactos();
-agregar("Mary Arroyo");
-agregar("Pablo Perez");
-imprimirContactos();
-borrarContacto("Ricardo Arcos");
-imprimirContactos();
+// forma 1- imprimir
+
+// function imprimeContacto(){
+//   contactList.forEach(function(newContact){
+//     console.log(newContact);
+// });
+// }
+
+// forma 2- for
+// function imprimeContacto(){
+//   for(let i=0; i<contactList.length; i++){
+//   console.log(contactList[i]);
+//   }
+// }
+
+// forma 3- join imprimir en una sola linea, es necesario el join
+
+  function imprimeContacto(){
+    console.log(contactList.join(", "));
+  }
+
+  // prueba 
+  console.log("Lista inicial de contactos: ")
+  imprimeContacto();
+  console.log("agregar contacto: ")
+  addContact("David Paez");
+  imprimeContacto();
+  console.log("eliminar contacto: ")
+  deleteContact('Cristian ruis');
+  imprimeContacto();
+
+  
