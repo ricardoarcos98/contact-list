f70e47a702966fc9de2b849f1fccbae361dca379
Ricardo Arcos <ricardoarcos98@hotmail.com>
2023-05-23 10:20:13 -0500

    entregable contactos mejorado

--- listadecontactos.js
+++ listadecontactos.js
@@ -1,113 +1,44 @@
- //ajuste lista de contactos 
+//ajuste lista de contactos 
  
- let contactList = [
-  {
-    id: 1,
-    nombres: "Ricardo",
-    apellidos: "Arcos",
-    telefono: "123456789",
-    ubicaciones: [
-      {
-        ciudad: "Bogota",
-        direccion: "calle 32-423"
-      },
-      {
-        ciudad: "Ciudad 2",
-        direccion: "calle 32-423"
-      }
-    ]
-  },
-  {
-    id: 2,
-    nombres: "Laura",
-    apellidos: "Castillejo",
-    telefono: "987654321",
-    ubicaciones: [
-      {
-        ciudad: "Ciudad 3",
-        direccion: "calle 32-423"
-      }
-    ]
-  },
-  {
-    id: 3,
-    nombres: "Luciana",
-    apellidos: "Arcos",
-    telefono: "567890123",
-    ubicaciones: [
-      {
-        ciudad: "Ciudad 5",
-        direccion: "Dirección 5"
-      }
-    ]
-  }
-];
-console.log(contactList);
+let contactList=[];
 
- // Crea una función para añadir un nuevo contacto a una lista.
+// Crea una función para añadir un nuevo contacto a una lista.
 
- function agregar(nuevoContacto) {
-  contactList.push(nuevoContacto);
+function addContact(nombres, apellidos, telefono, ciudad, direccion){
+    let contact ={ id:new Date().getTime(),
+                  nombres: nombres,
+                  apellidos: apellidos,
+                  telefono: telefono,
+                  ubicacion:{ ciudad:ciudad,
+                              direccion:direccion}
+                 }
+    contactList.push(contact); // se envia la información actualizada del contacto 
 }
 
-
-  // Crea una función para borrar un contacto existente de la lista
-  function borrarContacto(id) {
-    const index = contactList.findIndex(contacto => contacto.id === id);
-    if (index !== -1) {
-      contactList.splice(index, 1);
-      console.log("Contacto borrado con éxito");
-    } else {
-      console.log("El contacto no existe en la lista");
+  
+    // Crea una función para borrar un contacto existente de la lista
+    function borrarContacto(id) {
+      const index = contactList.findIndex(contacto => contacto.id === id);
+      if (index !== -1) {
+        contactList.splice(index, 1);
+        console.log("Contacto borrado con éxito");
+      } else {
+        console.log("El contacto no existe en la lista");
+      }
     }
-  }
-
-// forma 3- join imprimir en una sola linea, es necesario el join
-
-function imprimirContactos() {
-  contactList.forEach(contacto => {
-    console.log(`ID: ${contacto.id}`);
-    console.log(`Nombres: ${contacto.nombres}`);
-    console.log(`Apellidos: ${contacto.apellidos}`);
-    console.log(`Teléfono: ${contacto.telefono}`);
-    console.log("Ubicaciones:");
-    contacto.ubicaciones.forEach(ubicacion => {
-      console.log(`- Ciudad: ${ubicacion.ciudad}`);
-      console.log(`  Dirección: ${ubicacion.direccion}`);
-    });
-    console.log("---------------------------------------");
-  });
+  
+// funcion para imprimir contacto.
+  
+function imprimirContacto(){
+    console.log(contactList);
 }
 
-  // prueba 
-  agregar({
-    id: 4,
-    nombres: "Mary",
-    apellidos: "Arroyo",
-    telefono: "654321987",
-    ubicaciones: [
-      {
-        ciudad: "Cartagena",
-        direccion: "calle 26#73-34"
-      }
-    ]
-  });
-  
-  agregar({
-    id: 5,
-    nombres: "Pablo",
-    apellidos: "Perez",
-    telefono: "012345678",
-    ubicaciones: [
-      {
-        ciudad: "Bogota",
-        direccion: "calle 163#08-29"
-      }
-    ]
-  });
-  
-  borrarContacto(5);
-  imprimirContactos();
-  
+// prueba 
 
+//addContact("Nicolas","Arcos",3128438532,"Bogota","calle 23.32-32");
+//addContact("carmen","alonso",556461641,"cali","calle 45-54.23")
+//addContact("Juan","Vela",645876,"Choco","calle 4# 31-22")
+//imprimirContacto();
+//borrarContacto(id);
+//actualizarContacto(id,"nombres","pep")
   
